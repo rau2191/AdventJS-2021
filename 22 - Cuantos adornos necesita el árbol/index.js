05a1ee7ddf42f6b2d1e44857ade35bcef6d3d5f0
Raul Maestre <rau2191@hotmail.com>
2021-12-27 00:40:37 +0100

    Reto 22 completado

---
+++ 22 - Cuantos adornos necesita el árbol/index.js
@@ -0,0 +1,55 @@
+function countDecorations(bigTree) {
+  if (bigTree == null) return 0;
+
+  const { value, left, right } = bigTree;
+
+  return value + countDecorations(left) + countDecorations(right);
+}
+
+const tree = {
+  value: 1, // el nodo raíz siempre es uno, porque es la estrella ⭐
+  left: {
+    value: 2, // el nodo izquierdo necesita dos decoraciones
+    left: null, // no tiene más ramas
+    right: null // no tiene más ramas
+  },
+  right: {
+    value: 3, // el nodo de la derecha necesita tres decoraciones
+    left: null, // no tiene más ramas
+    right: null // no tiene más ramas
+  }
+};
+
+console.log(countDecorations(tree)); // 6
+
+const bigTree = {
+  value: 1,
+  left: {
+    value: 5,
+    left: {
+      value: 7,
+      left: {
+        value: 3,
+        left: null,
+        right: null
+      },
+      right: null
+    },
+    right: null
+  },
+  right: {
+    value: 6,
+    left: {
+      value: 5,
+      left: null,
+      right: null
+    },
+    right: {
+      value: 1,
+      left: null,
+      right: null
+    }
+  }
+};
+
+console.log(countDecorations(bigTree)); // 28
