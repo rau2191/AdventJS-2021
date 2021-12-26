c257d0d8811e57eb2a16e5114c5f25b27c5e690d
Raul Maestre <rau2191@hotmail.com>
2021-12-26 23:16:34 +0100

    Reto 7 completado

---
+++ 07 - Buscando en el almacén/index.js
@@ -0,0 +1,38 @@
+function contains(store, product) {
+  const values = Object.values(store);
+
+  return values.includes(product)
+    ? true
+    : values.some(p => typeof p === 'object' && contains(p, product))
+}
+
+const almacen = {
+  'estanteria1': {
+    'cajon1': {
+      'producto1': 'coca-cola',
+      'producto2': 'fanta',
+      'producto3': 'sprite'
+    }
+  },
+  'estanteria2': {
+    'cajon1': 'vacio',
+    'cajon2': {
+      'producto1': 'pantalones',
+      'producto2': 'camiseta' // <- ¡Está aquí!
+    }
+  }
+}
+
+console.log(contains(almacen, 'camiseta')) //true
+
+const otroAlmacen = {
+  'baul': {
+    'fondo': {
+      'objeto': 'cd-rom',
+      'otro-objeto': 'disquette',
+      'otra-cosa': 'mando'
+    }
+  }
+}
+
+console.log(contains(otroAlmacen, 'gameboy')) // false
